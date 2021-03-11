b96da25da2680e33710b227b82fc7d68b720fceb
braian <braiantalleres2015@gmail.com>
2021-03-11 17:22:24 -0300

    Agregar archivos de proyecto.

---
+++ Final Project Web-Panaderia/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_Project_Web_Panaderia.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
